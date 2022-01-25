412a170068b7dc0cccdf44ecc057c6e64915008a
guanzejie <424069281@QQ.com>
2022-01-25 23:10:15 +0800

    add

---
+++ PyQt_46_QAbstractScrollArea_抽象的滚动区域.py
@@ -0,0 +1,23 @@
+
+from PyQt5.Qt import *
+import sys
+app=QApplication(sys.argv)
+
+
+window=QWidget()
+window.setWindowTitle("光标控制")
+window.resize(500,500)
+
+te=QTextEdit(window)
+te.resize(100,100)
+
+te.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOn)    #设置垂直滚动条
+te.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOn)  #设置水平滚动条
+#Qt.ScrollBarAlwaysOff   #滚动条永久关闭
+#Qt.ScrollBarAlwaysOn    #滚动条永久开启
+
+bt=QPushButton(window)
+te.setCornerWidget(bt)  #边角处设置一个控件
+
+window.show()
+sys.exit(app.exec_())
