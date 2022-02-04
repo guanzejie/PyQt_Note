e23259fc32a31161422d938418a570128ac2ba55
guanzejie <424069281@QQ.com>
2022-02-04 22:30:43 +0800

    add12

---
+++ PyQt_55_QAbstractSpinBox_单行文本带按钮.py
@@ -0,0 +1,97 @@
+
+from PyQt5.Qt import *
+import sys
+
+app=QApplication(sys.argv)
+
+class MyASB(QAbstractSpinBox):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.lineEdit().setText("0")
+
+    def stepEnabled(self):                          #重写按钮，用户点击按钮的时候会自动调用这个方法
+        # if (int(self.text()) == 0 ):
+        #     return QAbstractSpinBox.StepUpEnabled   #按钮启动 +1
+        #
+        # elif (int(self.text()) == 9):
+        #     return QAbstractSpinBox.StepDownEnabled  #按钮启动 -1
+        #
+        # elif (int(self.text()) < 0 or int(self.text()) > 9):
+        #     return QAbstractSpinBox.StepNone         #关闭按钮
+
+        #else:
+            return QAbstractSpinBox.StepUpEnabled | QAbstractSpinBox.StepDownEnabled   #启动按钮
+
+    def stepBy(self, p_int):                          #重写接收按钮的数字
+        #print(p_int)
+        current_num=int(self.text())+p_int
+        self.lineEdit().setText(str(current_num))     #设置数值
+
+
+
+    def validate(self, p_str, p_int):      #数值验证器
+
+        #10-100
+        num=int(p_str)
+        if num<10:
+            return (QValidator.Intermediate,p_str, p_int)
+
+        elif num<=180:
+            return (QValidator.Acceptable, p_str, p_int)
+
+        else:
+            return (QValidator.Invalid, p_str, p_int)
+
+    def fixup(self, p_str):
+        print(p_str)
+        return "18"
+
+
+
+
+
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("test")
+        self.resize(500, 500)
+        self.setupUi()
+
+    def setupUi(self):
+
+        self.abs = MyASB(self)
+        self.abs.resize(200,50)
+        self.abs.move(200,200)
+
+        self.but=QPushButton(self)
+        self.but.setText("按钮")
+        self.but.pressed.connect(self.but_test)
+
+        self.abs.setAccelerated(True)              #长按是否加速
+        #self.abs.setReadOnly(True)                 #设置只读
+
+        cl=QCompleter(["1", "45", "75"], self.abs)    #设置快速查找
+        self.abs.lineEdit().setCompleter(cl)
+
+        self.abs.setAlignment(Qt.AlignCenter)          #设置对齐方式
+
+        self.abs.setFrame(True)                        #设置边框
+
+        #self.abs.clear()           #清空内容
+
+        self.abs.editingFinished.connect(lambda :print("结束编辑"))    #结束编辑信号
+
+    def but_test(self):
+
+        print(self.abs.text())               #打印数值
+        print(self.abs.lineEdit().text())
+        #self.abs.lineEdit().setText("100")    #设置数值
+
+
+
+
+window=Window()
+window.show()
+sys.exit(app.exec_())
---
+++ PyQt_57_QDoubleSpinBox单行数字文本框带按钮.py
@@ -0,0 +1,62 @@
+
+from PyQt5.Qt import *
+import sys
+
+app=QApplication(sys.argv)
+
+class Window(QWidget):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("test")
+        self.resize(500, 500)
+        self.setupUi()
+
+    def setupUi(self):
+        self.qsb=QDoubleSpinBox(self)
+        self.qsb.resize(100,30)
+        self.qsb.move(200,200)
+
+        #设置范围
+        #self.qsb.setMaximum(100.00)        # 设置最大值
+        #self.qsb.setMinimum(-100.00)       # 设置最小值
+        self.qsb.setAccelerated(True)     # 设置可以加速
+
+        self.qsb.setRange(-10.00,10.00)         #最大最小一起设置
+        self.qsb.setWrapping(True)        #设置数值在最大和最小中循环
+
+        self.qsb.setSingleStep(0.01)        #设置步长
+
+        #设置前缀和后缀
+        #self.qsb.setRange(0,6)
+        self.qsb.setPrefix("￥")             #前缀
+        self.qsb.setSuffix("%")              #后缀
+        #self.qsb.setSpecialValueText("周日")  #如果是最小值就调用这个
+
+        #self.qsb.setDisplayIntegerBase(2)     #设置显示为二禁止
+
+
+
+        #设置小数的位数
+        self.qsb.setDecimals(5)
+
+
+        self.qsb.setValue(0.00523)       #设置值为6
+        print(self.qsb.value())          #打印值
+        print(self.qsb.cleanText())      #只获取数值部分，不会四舍五入
+        print(self.qsb.lineEdit().text())#打印文本框的内容，包括前缀后缀
+
+
+        #可使用信号
+        self.qsb.valueChanged.connect(lambda : print("改变了"))
+
+
+
+
+
+
+
+
+window=Window()
+window.show()
+sys.exit(app.exec_())
